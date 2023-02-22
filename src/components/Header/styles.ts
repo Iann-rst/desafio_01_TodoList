bff193ed4c0047b6b331972276e0c4113f05030c
Iann Rodrigues <iannrst11@gmail.com>
2023-02-22 13:42:28 -0300

    feat: Componente Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 70,
+    backgroundColor: '#0D0D0D'
+  }
+})
